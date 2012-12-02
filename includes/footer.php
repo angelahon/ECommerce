f92d6ffded81c57fe36ac0b27ab735cbbd8ecd6a
Angela <anhelax3@gmail.com>
2012-12-01 19:38:43 -0500

    Added Files
    index.php
    override.css
    footer.php
    jquery.blueberry.js

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+    <div id="footer">
+        <ul>
+        <li><a href="#"><b>ContactUs /</b></a></li>
+        <li><a href="#"><b>Locations /</b></a></li>
+        <li><a href="#"><b>Shipping&Tracking /</b></a></li>
+        <li><a href="#"><b>MyAccount /</b></a></li>
+        <li><a href="#"><b>FAQs</b></a></li>
+        </ul>
+        <div align="right">
+        <a href="www.facebook.com"><img src="http://tctechcrunch2011.files.wordpress.com/2012/07/facebook-icon-1.png?w=300" style="margin-top:-30px;" width="30" height="30"/>  Follow us on <b style="color:#426CFF;">Facebook</b> for updates</a><br/>
+        <a href="www.twitter.com"><img src="https://twitter.com/images/resources/twitter-bird-light-bgs.png" style="margin-bottom:-15px;" width="40" height="40"/> Tweet about us on <b style="color:#39F;">Twitter</b></a>
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>RU Crazy?</title>
+	<!-- Stylesheets
+	============================================= -->
+    <link type="text/css" rel="stylesheet" href="includes/css/override.css" />
+    <!-- Javascripts
+	============================================= -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+    <script type="text/javascript" src="includes/js/jquery.blueberry.js"></script>
+    <!-- Functions
+	============================================= -->
+    <script>
+	$(window).load(function() {
+		$('.blueberry').blueberry();
+	});
+	</script>
+</head>
+<body>
+	<div id="wrapper"><a href="#">login</a> | shopping cart (<a href="#">#</a>)<br/><a href="#">new customer?</a>
+
+    <div id="header">RU Crazy</div>
+    <div id="navbar">
+    <ul>
+        <li><a href="#"><b>Home</b></a></li>
+        <li><a href="#"><b>Mens</b></a></li>
+        <li><a href="#"><b>Womens</b></a></li>
+        <li><a href="#"><b>Sports-Gear</b></a></li>
+        <li><a href="#"><b>Brands</b></a></li>
+        <li><a href="#"><b>Miscellaneous</b></a></li>
+        <li><a href="#"><b>Sale</b></a></li>
+        <li><a href="#"><b>Services</b></a></li>
+        <li><a href="#"><b>About</b></a></li>
+    </ul>
+	</div>
+  
+    <div id="doc">
+	<div id="content">
+
+	<!-- #start-blueberry-slideshow -->
+    <div class="blueberry">
+      <ul class="slides">
+        <li><img src="http://marktyrrell.com/labs/blueberry/img/slide1.jpg" /></li>
+        <li><img src="http://marktyrrell.com/labs/blueberry/img/slide2.jpg" /></li>
+        <li><img src="http://marktyrrell.com/labs/blueberry/img/slide3.jpg" /></li>
+        <li><img src="http://marktyrrell.com/labs/blueberry/img/slide4.jpg" /></li>
+      </ul>
+    </div><!-- #end-blueberry -->
+    
+	</div>
+    </div>
+    
+    <?php include"includes/footer.php"?>
+    
+	</div>
+</body>
+</html>
